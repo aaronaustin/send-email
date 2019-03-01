9be18c9fc2992deb7a41d53c1dba62af7f09bdd0
Aaron Austin <aaron@aaronaustin.com>
2019-02-28 22:55:09 -0500

    fix

--- send-email.php
+++ send-email.php
@@ -1,7 +1,7 @@
 <?php
 /*Plugin Name: Send Email
 Description: Send email from api.
-Version: 1.2.0
+Version: 1.2.1
 License: GPLv2
 GitHub Plugin URI: https://github.com/aaronaustin/send-email
 */
@@ -12,7 +12,7 @@ function sendMailWithWP (WP_REST_Request $request) {
     $email = $request['email'];
     $subject = $request['subject'];
     $headers = 'From: '. 'info@lexcentral.com' . "\r\n" . 'Reply-To: ' . $email . "\r\n";
-    $message = $request['message'] . "\r\n" . "Message From: " . $name . "<" . $email . ">";
+    $message = $request['message'] . "\r\n" . "Message From: " . $name . "&lt;" . $email . "&gt;";
     
     $sent = wp_mail($to, $subject, strip_tags($message));
     
